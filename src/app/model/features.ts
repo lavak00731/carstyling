31b0642c1e1066c591bd3cae1c0ac99d41d9a811
Lavaca <ezequiel.o.lavaca@accenture.com>
2020-08-21 09:25:48 -0300

    adding api partially

---
+++ src/app/model/features.ts
@@ -0,0 +1,51 @@
+export class Features {
+    [
+        {
+            'van': {
+                'pictures': {
+                    '45': {
+                        'base': 'van/45/45base.jpg',
+                        'hood': ['van/45/45-hoodblue.png', 'van/45/45-hoodbone.png', 'van/45/45-hoodliver.png'],
+                        'roof': ['van/45/45-roofblue.png', 'van/45/45-roofbone.png', 'van/45/45-roofliver.png'],
+                        'sides': ['van/45/45-sidesblue.png', 'van/45/45-sidesbone.png', 'van/45/45-liver.png']
+                    },
+                    'back': {
+                        'base': 'van/back/backbase.jpg',
+                        'back': ['van/back/backblue.png', 'van/back/backbone.png', 'van/back/backliver.png']
+                    },
+                    'front': {
+                        'base': 'van/front/frontbase.jpg',
+                        'hood': ['van/front/front-hoodblue.png', 'van/front/front-hoodbone.png', 'van/front/front-hoodliver.png'],
+                        'roof': ['van/front/front-roofblue.png', 'van/front/front-roofbone.png', 'van/front/front-roofliver.png'],
+                        'sides': ['van/front/front-sidesblue.png', 'van/front/front-sidesbone.png', 'van/front/front-sidesliver.png']
+                    },
+                    'left': {
+                        'base': 'van/left/leftbase.jpg',
+                        'left': ['van/left/left-blue.png', 'van/left/left-bone.png', 'van/left/left-liver.png']
+                    },
+                    'right': {
+                        'base': 'van/right/rightbase.jpg',
+                        'right': ['van/right/right-blue.png', 'van/right/right-bone.png', 'van/right/right-liver.png']
+                    },
+                    'top': {
+                        'base': 'van/top/topbase.jpg',
+                        'hood': ['van/top/top-hoodblue.png', 'van/top/top-hoodbone.png', 'van/top/top-hoodliver.png'],
+                        'roof': ['van/top/top-roofblue.png', 'van/top/top-roofbone.png', 'van/top/top-roofliver.png'],
+                        'sides': ['van/top/top-sidesblue.png', 'van/top/top-sidesbone.png', 'van/top/top-sidesliver.png']
+                    }
+                },
+                'features': {
+                    'front': ['#247BA0', '#E0DDCF', '#534B52'],
+                    'hood': ['#247BA0', '#E0DDCF', '#534B52'],
+                    'back': ['#247BA0', '#E0DDCF', '#534B52'],
+                    'sides': ['#247BA0', '#E0DDCF', '#534B52'],
+                    'roof': ['#247BA0', '#E0DDCF', '#534B52']
+                }
+                
+            }
+        },
+        {
+
+        }
+    ]
+}
