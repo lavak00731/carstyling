78e23e10f336492d8ef8e2c47ad6ca0fe7306033
Lavaca <ezequiel.o.lavaca@accenture.com>
2020-08-26 11:43:29 -0300

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { CustomizeComponent } from './customize/customize.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { FinalComponent } from './final/final.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomizeComponent,
+    ConfirmComponent,
+    FinalComponent
   ],
   imports: [
     BrowserModule,
--- src/app/model/features.ts
+++ src/app/model/features.ts
@@ -1,91 +1,112 @@
 export class Features {
     [
         {
-            'van': {
-                'pictures': {
-                    '45': {
-                        'base': 'van/45/45base.jpg',
-                        'hood': ['van/45/45-hoodblue.png', 'van/45/45-hoodbone.png', 'van/45/45-hoodliver.png'],
-                        'roof': ['van/45/45-roofblue.png', 'van/45/45-roofbone.png', 'van/45/45-roofliver.png'],
-                        'sides': ['van/45/45-sidesblue.png', 'van/45/45-sidesbone.png', 'van/45/45-liver.png']
+            "van": {
+                "pictures": {
+                    "45": {
+                        "base": "van/45/45base.jpg",
+                        "hood": ["van/45/45-hoodblue.png", "van/45/45-hoodbone.png", "van/45/45-hoodliver.png"],
+                        "roof": ["van/45/45-roofblue.png", "van/45/45-roofbone.png", "van/45/45-roofliver.png"],
+                        "sides": ["van/45/45-sidesblue.png", "van/45/45-sidesbone.png", "van/45/45-liver.png"]
                     },
-                    'back': {
-                        'base': 'van/back/backbase.jpg',
-                        'back': ['van/back/backblue.png', 'van/back/backbone.png', 'van/back/backliver.png']
+                    "back": {
+                        "base": "van/back/backbase.jpg",
+                        "back": ["van/back/backblue.png", "van/back/backbone.png", "van/back/backliver.png"]
                     },
-                    'front': {
-                        'base': 'van/front/frontbase.jpg',
-                        'hood': ['van/front/front-hoodblue.png', 'van/front/front-hoodbone.png', 'van/front/front-hoodliver.png'],
-                        'roof': ['van/front/front-roofblue.png', 'van/front/front-roofbone.png', 'van/front/front-roofliver.png'],
-                        'sides': ['van/front/front-sidesblue.png', 'van/front/front-sidesbone.png', 'van/front/front-sidesliver.png']
+                    "front": {
+                        "base": "van/front/frontbase.jpg",
+                        "hood": ["van/front/front-hoodblue.png", "van/front/front-hoodbone.png", "van/front/front-hoodliver.png"],
+                        "roof": ["van/front/front-roofblue.png", "van/front/front-roofbone.png", "van/front/front-roofliver.png"],
+                        "sides": ["van/front/front-sidesblue.png", "van/front/front-sidesbone.png", "van/front/front-sidesliver.png"]
                     },
-                    'left': {
-                        'base': 'van/left/leftbase.jpg',
-                        'left': ['van/left/left-blue.png', 'van/left/left-bone.png', 'van/left/left-liver.png']
+                    "left": {
+                        "base": "van/left/leftbase.jpg",
+                        "left": ["van/left/left-blue.png", "van/left/left-bone.png", "van/left/left-liver.png"]
                     },
-                    'right': {
-                        'base': 'van/right/rightbase.jpg',
-                        'right': ['van/right/right-blue.png', 'van/right/right-bone.png', 'van/right/right-liver.png']
+                    "right": {
+                        "base": "van/right/rightbase.jpg",
+                        "right": ["van/right/right-blue.png", "van/right/right-bone.png", "van/right/right-liver.png"]
                     },
-                    'top': {
-                        'base': 'van/top/topbase.jpg',
-                        'hood': ['van/top/top-hoodblue.png', 'van/top/top-hoodbone.png', 'van/top/top-hoodliver.png'],
-                        'roof': ['van/top/top-roofblue.png', 'van/top/top-roofbone.png', 'van/top/top-roofliver.png'],
-                        'sides': ['van/top/top-sidesblue.png', 'van/top/top-sidesbone.png', 'van/top/top-sidesliver.png']
+                    "top": {
+                        "base": "van/top/topbase.jpg",
+                        "hood": ["van/top/top-hoodblue.png", "van/top/top-hoodbone.png", "van/top/top-hoodliver.png"],
+                        "roof": ["van/top/top-roofblue.png", "van/top/top-roofbone.png", "van/top/top-roofliver.png"],
+                        "sides": ["van/top/top-sidesblue.png", "van/top/top-sidesbone.png", "van/top/top-sidesliver.png"]
                     }
                 },
-                'features': {
-                    'front': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'hood': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'back': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'sides': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'roof': ['#247BA0', '#E0DDCF', '#534B52']
+                "features": {
+                    "front": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "hood": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "back": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "sides": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "roof": ["#247BA0", "#E0DDCF", "#534B52"]
+                },
+                "cardata": {
+                    "Name":"Dodge d200",
+                    "Miles_per_Gallon":11,
+                    "Cylinders":8,
+                    "Horsepower":210,
+                    "Weight_in_lbs":4382,
+                    "Acceleration":13.5,
+                    "Year":"2020",
+                    "Origin":"USA",
+                    "picture": "van/45/45base.jpg"
                 }
-                
             }
         },
         {
-            'bigvan': {
-                'pictures': {
-                    '45': {
-                        'base': 'bigvan/45/45base.jpg',
-                        'front': ['bigvan/45/front-blue45.png', 'bigvan/45/front-bone45.png', 'bigvan/45/front-liver45.png']
-                        'hood': ['bigvan/45/hood-blue45.png', 'bigvan/45/hood-bone45.png', 'bigvan/45/hood-liver45.png'],
-                        'roof': ['bigvan/45/roof-blue45.png', 'bigvan/45/roof-bone45.png', 'bigvan/45/roof-liver45.png'],
-                        'sides': ['bigvan/45/side-blue45.png', 'bigvan/45/side-bone45.png', 'bigvan/45/side-liver45.png']
+            "bigvan": {
+                "pictures": {
+                    "45": {
+                        "base": "bigvan/45/45base.jpg",
+                        "front": ["bigvan/45/front-blue45.png", "bigvan/45/front-bone45.png", "bigvan/45/front-liver45.png"]
+                        "hood": ["bigvan/45/hood-blue45.png", "bigvan/45/hood-bone45.png", "bigvan/45/hood-liver45.png"],
+                        "roof": ["bigvan/45/roof-blue45.png", "bigvan/45/roof-bone45.png", "bigvan/45/roof-liver45.png"],
+                        "sides": ["bigvan/45/side-blue45.png", "bigvan/45/side-bone45.png", "bigvan/45/side-liver45.png"]
                     },
-                    'back': {
-                        'base': 'bigvan/back/backbase.jpg',
-                        'back': ['bigvan/back/backblue.png', 'bigvan/back/backbone.png', 'bigvan/back/backliver.png']
+                    "back": {
+                        "base": "bigvan/back/backbase.jpg",
+                        "back": ["bigvan/back/backblue.png", "bigvan/back/backbone.png", "bigvan/back/backliver.png"]
                     },
-                    'front': {
-                        'base': 'bigvan/front/frontbase.jpg',
-                        'front': ['bigvan/front/front-blue.png', 'bigvan/front/front-bone.png', 'bigvan/front/front-liver.png']
-                        'hood': ['bigvan/front/front-hoodblue.png', 'bigvan/front/front-hoodbone.png', 'bigvan/front/front-hoodliver.png'],
-                        'roof': ['bigvan/front/front-roofblue.png', 'bigvan/front/front-roofbone.png', 'bigvan/front/front-roofliver.png'],
-                        'sides': ['bigvan/front/front-sidesblue.png', 'bigvan/front/front-sidesbone.png', 'bigvan/front/front-sidesliver.png']
+                    "front": {
+                        "base": "bigvan/front/frontbase.jpg",
+                        "front": ["bigvan/front/front-blue.png", "bigvan/front/front-bone.png", "bigvan/front/front-liver.png"]
+                        "hood": ["bigvan/front/front-hoodblue.png", "bigvan/front/front-hoodbone.png", "bigvan/front/front-hoodliver.png"],
+                        "roof": ["bigvan/front/front-roofblue.png", "bigvan/front/front-roofbone.png", "bigvan/front/front-roofliver.png"],
+                        "sides": ["bigvan/front/front-sidesblue.png", "bigvan/front/front-sidesbone.png", "bigvan/front/front-sidesliver.png"]
                     },
-                    'left': {
-                        'base': 'bigvan/left/leftbase.jpg',
-                        'left': ['bigvan/left/left-blue.png', 'bigvan/left/left-bone.png', 'bigvan/left/left-liver.png']
+                    "left": {
+                        "base": "bigvan/left/leftbase.jpg",
+                        "left": ["bigvan/left/left-blue.png", "bigvan/left/left-bone.png", "bigvan/left/left-liver.png"]
                     },
-                    'right': {
-                        'base': 'bigvan/right/rightbase.jpg',
-                        'right': ['bigvan/right/right-blue.png', 'bigvan/right/right-bone.png', 'bigvan/right/right-liver.png']
+                    "right": {
+                        "base": "bigvan/right/rightbase.jpg",
+                        "right": ["bigvan/right/right-blue.png", "bigvan/right/right-bone.png", "bigvan/right/right-liver.png"]
                     },
-                    'top': {
-                        'base': 'bigvan/top/topbase.jpg',
-                        'hood': ['bigvan/top/top-hoodblue.png', 'bigvan/top/top-hoodbone.png', 'bigvan/top/top-hoodliver.png'],
-                        'roof': ['bigvan/top/top-roofblue.png', 'bigvan/top/top-roofbone.png', 'bigvan/top/top-roofliver.png'],
-                        'sides': ['bigvan/top/top-sidesblue.png', 'bigvan/top/top-sidesbone.png', 'bigvan/top/top-sidesliver.png']
+                    "top": {
+                        "base": "bigvan/top/topbase.jpg",
+                        "hood": ["bigvan/top/top-hoodblue.png", "bigvan/top/top-hoodbone.png", "bigvan/top/top-hoodliver.png"],
+                        "roof": ["bigvan/top/top-roofblue.png", "bigvan/top/top-roofbone.png", "bigvan/top/top-roofliver.png"],
+                        "sides": ["bigvan/top/top-sidesblue.png", "bigvan/top/top-sidesbone.png", "bigvan/top/top-sidesliver.png"]
                     }
                 },
-                'features': {
-                    'front': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'hood': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'back': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'sides': ['#247BA0', '#E0DDCF', '#534B52'],
-                    'roof': ['#247BA0', '#E0DDCF', '#534B52']
+                "features": {
+                    "front": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "hood": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "back": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "sides": ["#247BA0", "#E0DDCF", "#534B52"],
+                    "roof": ["#247BA0", "#E0DDCF", "#534B52"]
+                },
+                "cardata": {
+                    "Name":"AMC gremlin",
+                    "Miles_per_Gallon":19,
+                    "Cylinders":6,
+                    "Horsepower":100,
+                    "Weight_in_lbs":2634,
+                    "Acceleration":13,
+                    "Year":"2020",
+                    "Origin":"USA",
+                    "picture": "bigvan/45/base45.jpg"
                 }
             }
         }
