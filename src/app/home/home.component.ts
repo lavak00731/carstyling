0b50fee61921ebdcc5a11b59ce4b29045d3a38fa
Ezequiel Lavaca <eolavaca@gmail.com>
2021-07-21 13:56:09 -0300

    Error handling using observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CarInterface } from '../model/car-interface';
 import { CarserviceService } from '../services/carservice.service';
 import { SelectionBasketService } from '../services/selection-basket.service';
 import {Router} from '@angular/router'; // import router from angular router
+import { catchError } from 'rxjs/operators';
+import { EMPTY, Subject } from 'rxjs';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
   constructor( private _carService: CarserviceService, private _selectionBasket: SelectionBasketService, private route:Router) { }
   cars: CarInterface[];
-  ngOnInit(): void {
-    this._carService.getCars().subscribe(res => {
-      this.cars = res;      
-    });    
-  }
-  carSelection(carId, carCardata) {
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+
+
+  cars$ =  this._carService.cars$
+            .pipe(
+              catchError( err =>{
+                this.errorMessageSubject.next(err);
+                return EMPTY;
+              })
+            )
+       
+  
+  private carSelection(carId: string, carCardata: string) {
     this._selectionBasket.updateCarSelection(carId, carCardata);
     this.route.navigate(['/customize']); // navigate to other page
   }
+
 }
--- src/app/services/carservice.service.ts
+++ src/app/services/carservice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CarInterface } from './../model/car-interface';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -11,8 +11,25 @@ export class CarserviceService {
 
   constructor(private http: HttpClient) { }
   private carsUrl = 'api/cars';  // URL to web api
-    
-  getCars(): Observable<CarInterface[]> {
-    return this.http.get<CarInterface[]>(this.carsUrl)
+
+  cars$ = this.http.get<CarInterface[]>(this.carsUrl) 
+    .pipe(
+      catchError(this.handleError)
+    );
+
+  private handleError(err: any) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    return throwError(errorMessage);
   }
+
 }
