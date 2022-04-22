c6229fc69337930bc6d6ddba1610eee351f34ba0
Ezequiel Lavaca <eolavaca@gmail.com>
2022-04-22 16:47:04 -0300

    validation process

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -68,7 +68,6 @@ export class ConfirmComponent implements OnInit, OnDestroy {
   } 
   ngOnInit(): void {
     this.getCar(+this._selection.getSelection()['id']);
-    console.log(Validators);
   }
 
   ngOnDestroy(): void {
@@ -90,15 +89,30 @@ export class ConfirmComponent implements OnInit, OnDestroy {
   //credit card number validation
   creditValidation(): ValidatorFn {
     return (control:AbstractControl) : ValidationErrors | null => {
-      const visaRegEx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
-      const mastercardRegEx = /^(?:5[1-5][0-9]{14})$/;
-      const amexpRegEx = /^(?:3[47][0-9]{13})$/;
-      const discovRegEx = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
+      const creditCards = {
+        amex: new RegExp('/^3[47]\d{13,14}$/'),
+        visa: new RegExp('^4[0-9]{12}(?:[0-9]{3})?$'),
+        cup1: new RegExp('^62[0-9]{14}[0-9]*$'),
+        cup2: new RegExp('^81[0-9]{14}[0-9]*$'),      
+        mastercard: new RegExp('^5[1-5][0-9]{14}$'),
+        mastercard2: new RegExp('^2[2-7][0-9]{14}$'),      
+        disco1: new RegExp('^6011[0-9]{12}[0-9]*$'),
+        disco2: new RegExp('^62[24568][0-9]{13}[0-9]*$'),
+        disco3: new RegExp('^6[45][0-9]{14}[0-9]*$'),        
+        diners: new RegExp('^3[0689][0-9]{12}[0-9]*$'),
+        jcb: new RegExp('^35[0-9]{14}[0-9]*$')
+      }
+         
       let isValid = false;
-      let valueFormated = control.value.replace(/\s+/g, '');
-      if (visaRegEx.test(valueFormated) || mastercardRegEx.test(valueFormated) || amexpRegEx.test(valueFormated) || discovRegEx.test(valueFormated)) {
-        isValid = true;
-      } 
+      let valueFormated = control.value.replace(/\s/g, '');
+      valueFormated = valueFormated.replace('_', '');
+      
+      for ( const cc in creditCards) {
+        if (creditCards[cc].test(valueFormated)) {
+          isValid = true;
+        }
+      }
+      console.log(isValid);
       return isValid ? {creditCardValid: true} : null;        
     }
   }
@@ -106,31 +120,31 @@ export class ConfirmComponent implements OnInit, OnDestroy {
   //user form data
   clientProfile = new FormGroup({
     personalData: new FormGroup({
-      firstname: new FormControl('', [Validators.required, Validators.pattern(/[a-zA-Z]{3,}/g)]),
-      lastname: new FormControl('', [Validators.required, Validators.pattern(/[a-zA-Z]{3,}/g)]),
+      firstname: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z\s]{3,}$/)]),
+      lastname: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z\s]{3,}$/)]),
       address: new FormControl('', Validators.required),
       phone: new FormControl('', [Validators.required, Validators.minLength(8)]),
       email: new FormControl('', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)])
     }),
     paymentMethod: new FormGroup({
       ccard: new FormControl('', [Validators.required, this.creditValidation()]),
-      cardholdername: new FormControl('', [Validators.required, Validators.pattern(/[a-zA-Z]{6,}/g)]),
+      cardholdername: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z\s]{6,}$/)]),
       expDate: new FormControl('', [Validators.required, this.expDateValid()]),
-      secNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{3,4}$/g)])
+      secNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{3,4}$/)])
     })
   })
   validateForm() {
     if(this.clientProfile.valid) {
       this.formValidity = false;
+      console.log(this.clientProfile);
     } else {
       this.formValidity = true;
       //focus on error banner
       setTimeout(() => {
         this.errorBanner.nativeElement.focus()
-      })
+      });
 
       for (const key in this.clientProfile.controls) {
-        //console.log(this.clientProfile.controls[key]['controls'])
         for ( const innerProp in this.clientProfile.controls[key]['controls']) {
           this.clientProfile.controls[key]['controls'][innerProp].markAsTouched();
         }
